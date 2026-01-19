0d65a154944469deaf26e61124a0fae86bf49972
NkemJefersonAchia <nkemachia05@gmail.com>
2026-01-19 20:24:39 +0100

    REST API

---
+++ Python_API/my_api.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True) ##keeps the api running
