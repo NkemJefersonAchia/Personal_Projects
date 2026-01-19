91b8908237f5d0a0c015ba329ae6fd2d6b8359c3
NkemJefersonAchia <nkemachia05@gmail.com>
2026-01-19 21:19:53 +0100

    REST API

--- Python_API/my_api.py
+++ Python_API/my_api.py
@@ -1,8 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+##Create Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///travel.db"
+
+db = SQLAlchemy(app)
+
+class Passenger(db.Model): ##row in a db
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "destination": self.destination,
+            "country": self.country,
+            "rating": self.rating
+            
+        }
+    
+with app.app_context():
+    db.create_all()
+
+
+##Random routes
+@app.route("/")
+def home():
+    return jsonify({"message": "welcome to the travel api"})
 
 
 
@@ -12,4 +40,4 @@ app = Flask(__name__)
 
 
 if __name__ == "__main__":
-    app.run(debug=True) ##keeps the api running
+    app.run(debug=True) ##keeps the api running when you refresh
