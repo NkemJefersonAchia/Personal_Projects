8953e50b313f5a58b2a55fe14560b06713fbe0ce
NkemJefersonAchia <nkemachia05@gmail.com>
2026-02-02 20:44:25 +0100

    variables

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -1,5 +1,17 @@
+
 // A variable is a container that stores a value 
 
 // We can create a variable by assignment and declaration
 // 1. declaration.  let x;
 // 2. assignment.   x=100;
+
+
+// we use the keyword "let" to declare variables. We can also declare and assign
+//  Use template literals for string interpolation in console output
+
+let age = 21; //number
+let userName = "Nkem Jeferson Achia";  //string
+let isEnrolled = true; //boolean
+ document.getElementById("name").textContent = `Name: ${userName}`;
+ document.getElementById("age").textContent = `Age: ${age}`;
+ document.getElementById("status").textContent = `Status: ${isEnrolled}`;
